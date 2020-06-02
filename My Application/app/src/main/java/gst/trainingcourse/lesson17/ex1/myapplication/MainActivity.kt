127d9d8c357adcf185663c5f259a192a54b5a69a
thuan1 <thuan1@gmail.com>
2020-06-02 21:20:30 +0700

    commit 3

--- My Application/app/src/main/java/gst/trainingcourse/lesson17/ex1/myapplication/MainActivity.kt
+++ My Application/app/src/main/java/gst/trainingcourse/lesson17/ex1/myapplication/MainActivity.kt
@@ -2,11 +2,20 @@ package gst.trainingcourse.lesson17.ex1.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        Toast.makeText(this, "toi la ai", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "toi la thuan01", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "toi la thuan03", Toast.LENGTH_SHORT).show()
     }
 }
